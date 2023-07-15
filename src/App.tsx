cdf9ad839aa3b10d4d710c41df5f042cb209557a
Hayley Ashby <hayleyashby7@gmail.com>
2023-07-15 14:36:46 +0100

    feat: :art: Adding new cat for purposes of activity 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import Navbar from './components/navbar';
 import Header from './components/header';
@@ -9,7 +10,16 @@ import CatCard from './components/cat_card';
 import images from './data/cat-images';
 
 function App(): JSX.Element {
-	const [cats, setCats] = useState<Array<Cat>>(catData);
+	const [cats, setCats] = useState<Array<Cat>>([
+		{
+			id: uuidv4(),
+			name: 'Captain Catface',
+			species: 'Sea Cattain',
+			favFoods: ['fish, rum'],
+			birthYear: 2016,
+		},
+		...catData,
+	]);
 
 	const matchImage = (id: number | undefined) => {
 		if (id === undefined) {
