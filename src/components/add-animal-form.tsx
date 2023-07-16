564a2a4e65baa25f59d9b6232b663526cea55e7b
Hayley Ashby <hayleyashby7@gmail.com>
2023-07-16 15:26:06 +0100

    feat: :sparkles: Add in form reset and simple validation

--- src/components/add-animal-form.tsx
+++ src/components/add-animal-form.tsx
@@ -14,30 +14,38 @@ interface AddAnimalFormProps {
 	onSubmit: (data: formInput) => void;
 }
 
-const toggleForm = () => {
-	const form = document.querySelector('.form');
-	form?.classList.toggle('form--closed');
-
-	const btn = document.querySelector('.btn--add');
-	if (btn) btn?.innerHTML === 'Add Animal' ? (btn.innerHTML = 'Close Form') : (btn.innerHTML = 'Add Animal');
-};
-
 const AddAnimalForm: React.FC<AddAnimalFormProps> = ({ onSubmit }) => {
-	const { register, handleSubmit } = useForm<formInput>();
+	const { register, handleSubmit, reset } = useForm<formInput>();
+
+	const submitForm = (data: formInput) => {
+		onSubmit(data);
+		toggleForm();
+	};
+
+	const toggleForm = () => {
+		const form = document.querySelector('.form');
+		form?.classList.toggle('form--closed');
+
+		const btn = document.querySelector('.btn--add');
+		if (btn) btn?.innerHTML === 'Add Animal' ? (btn.innerHTML = 'Close Form') : (btn.innerHTML = 'Add Animal');
+
+		reset();
+	};
 
 	return (
 		<div className='form__wrapper'>
 			<Button btnClassNames='btn card__text btn--add' onClick={() => toggleForm()} text='Add Animal' />
-			<form className='card form form--closed' onSubmit={handleSubmit(onSubmit)}>
+			<form className='card form form--closed' onSubmit={handleSubmit(submitForm)}>
 				<label className='card__text' htmlFor='name'>
 					Name
 				</label>
-				<input className='card__text' id='name' {...register('name')} autoComplete='off' />
+				<input className='card__text' id='name' {...register('name')} required />
 
 				<label className='card__text' htmlFor='animal'>
 					Type of Animal
 				</label>
-				<select className='card__text' id='animal' {...register('animal')}>
+				<select className='card__text' id='animal' {...register('animal')} required>
+					<option value=''>Select an animal</option>
 					<option value='cat'>Cat</option>
 					<option value='dog'>Dog</option>
 				</select>
@@ -45,17 +53,17 @@ const AddAnimalForm: React.FC<AddAnimalFormProps> = ({ onSubmit }) => {
 				<label className='card__text' htmlFor='species'>
 					Species
 				</label>
-				<input className='card__text' id='species' {...register('species')} />
+				<input className='card__text' id='species' {...register('species')} required />
 
 				<label className='card__text' htmlFor='favFoods'>
 					Favourite Foods
 				</label>
-				<input className='card__text' id='favFoods' {...register('favFoods')} />
+				<input className='card__text' id='favFoods' {...register('favFoods')} required />
 
 				<label className='card__text' htmlFor='birthYear'>
 					Birth Year
 				</label>
-				<input className='card__text' id='birthYear' {...register('birthYear')} />
+				<input className='card__text' id='birthYear' {...register('birthYear')} type='number' required />
 
 				<input className='card__text btn' type='submit' />
 			</form>
