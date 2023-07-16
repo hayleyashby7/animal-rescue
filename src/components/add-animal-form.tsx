df90fb9f6ce55dbcdaf50fc2c5c05cca263fa8ed
Hayley Ashby <hayleyashby7@gmail.com>
2023-07-16 14:13:25 +0100

    feat: :sparkles: Add animal form is hidden by default. Display visibility can be toggled by button

--- src/components/add-animal-form.tsx
+++ src/components/add-animal-form.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import Button from './button';
 
 interface formInput {
 	name: string;
@@ -13,31 +14,52 @@ interface AddAnimalFormProps {
 	onSubmit: (data: formInput) => void;
 }
 
+const toggleForm = () => {
+	const form = document.querySelector('.form');
+	form?.classList.toggle('form--closed');
+
+	const btn = document.querySelector('.btn--add');
+	if (btn) btn?.innerHTML === 'Add Animal' ? (btn.innerHTML = 'Close Form') : (btn.innerHTML = 'Add Animal');
+};
+
 const AddAnimalForm: React.FC<AddAnimalFormProps> = ({ onSubmit }) => {
 	const { register, handleSubmit } = useForm<formInput>();
 
 	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<label htmlFor='name'>Name</label>
-			<input id='name' {...register('name')} autoComplete='false' />
+		<div className='form__wrapper'>
+			<Button btnClassNames='btn card__text btn--add' onClick={() => toggleForm()} text='Add Animal' />
+			<form className='card form form--closed' onSubmit={handleSubmit(onSubmit)}>
+				<label className='card__text' htmlFor='name'>
+					Name
+				</label>
+				<input className='card__text' id='name' {...register('name')} autoComplete='off' />
 
-			<label htmlFor='animal'>Type of Animal</label>
-			<select id='animal' {...register('animal')}>
-				<option value='cat'>Cat</option>
-				<option value='dog'>Dog</option>
-			</select>
+				<label className='card__text' htmlFor='animal'>
+					Type of Animal
+				</label>
+				<select className='card__text' id='animal' {...register('animal')}>
+					<option value='cat'>Cat</option>
+					<option value='dog'>Dog</option>
+				</select>
 
-			<label htmlFor='species'>Species</label>
-			<input id='species' {...register('species')} />
+				<label className='card__text' htmlFor='species'>
+					Species
+				</label>
+				<input className='card__text' id='species' {...register('species')} />
 
-			<label htmlFor='favFoods'>Favourite Foods</label>
-			<input id='favFoods' {...register('favFoods')} />
+				<label className='card__text' htmlFor='favFoods'>
+					Favourite Foods
+				</label>
+				<input className='card__text' id='favFoods' {...register('favFoods')} />
 
-			<label htmlFor='birthYear'>Birth Year</label>
-			<input id='birthYear' {...register('birthYear')} />
+				<label className='card__text' htmlFor='birthYear'>
+					Birth Year
+				</label>
+				<input className='card__text' id='birthYear' {...register('birthYear')} />
 
-			<input type='submit' />
-		</form>
+				<input className='card__text btn' type='submit' />
+			</form>
+		</div>
 	);
 };
 
